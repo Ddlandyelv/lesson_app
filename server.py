c4d105b49c1c8e8fb12c6dfaa4079e06d63937e9
郑书豪 <2302813648@qq.com>
2026-05-30 16:30:59 +0800

    server.py 启动时自动打开浏览器

--- server.py
+++ server.py
@@ -122,4 +122,6 @@ def download():
     return send_file(output_path, as_attachment=True)
 
 if __name__ == "__main__":
+    import webbrowser
+    webbrowser.open("http://127.0.0.1:5000")
     app.run(host="127.0.0.1", port=5000, debug=True)
